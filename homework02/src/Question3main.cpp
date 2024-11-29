c3d5f837c0c6f2b33c9c2840c5281ab5ecb17f4d
galxhad <dms7637@psu.edu>
2024-11-29 00:10:23 -0500

    Delete homework02 directory

--- homework02/src/Question3main.cpp
+++
@@ -1,8 +0,0 @@
-#include "airport_simulator.h"
-
-int main() 
-{
-    simulate_aircraft_arrival();  // Start simulating aircraft arrivals
-    std::this_thread::sleep_for(std::chrono::seconds(5));  // Give enough time for all threads to complete
-    return 0;
-}
